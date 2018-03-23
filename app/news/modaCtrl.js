c7cc22c0fdba10082e56aabb8c754e3507c4d9ef
frontend.moises@gmail.com <frontend.moises@gmail.com>
2018-03-23 02:49:32 -0300

    hhbb

--- app/news/modaCtrl.js
+++ app/news/modaCtrl.js
@@ -4,34 +4,7 @@
 				var me= this;
 
 				me.moda=[
-				            {
-				                "idNoticia": 1,
-				                "codigoNoticia": "NWD-9838",
-				                "tituloNoticia": "Desfiles en viña del mar Chile",
-				                "descripcionNoticia": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dolor,eos laborum quaerat quam quos rerum!" +
-				                "Expedita itaque optio praesentium quo sequi soluta voluptatem! At eos est fuga illum iusto necessitatibus " +
-				                "bcaecati quia tempora ullam. Commodi doloribus error ex fugiat laborum magni, officiis ratione reiciendis sunt" +
-				                "velit voluptas voluptates. Culpa deleniti dolore, excepturi fugit id in sed vitae voluptates. Tenetur.",
-				                "fechaPublicacion": new Date(),
-				                "banner" : "http://cde.3.elcomercio.pe/ima/0/1/2/3/4/1234815/base_image.jpg",
-				                "destacado": true,
-				                "categoria": "Politica",
-				                "tags": ["peru", "politica", "guerra"]
-				            },
-				            {
-				                "idNoticia": 2,
-				                "codigoNoticia": "NFK-1275",
-				                "tituloNoticia": "Federer jugo contra Nadal",
-				                "descripcionNoticia": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Al quaerat quam quos rerum!" +
-				                "Expedita itaque Commodi doloribus error ex fugiat laborum mag At eos est fuga illum iusto necessitatibus " +
-				                "bcaecati quia tempora ullam. Commodi doloribus error ex fugiat laborum magni, officiis ratione reiciendis sunt" +
-				                "velit voluptas voluptates. Dst fuga illum iusto necessita in sed vitae voluptates. Tenetur.",
-				                "fechaPublicacion": new Date(2015,0,1),
-				                "banner" : "http://cde.3.elcomercio.pe/ima/0/1/2/3/5/1235147/base_image.jpg",
-				                "destacado": true,
-				                "categoria": "Politica",
-				                "tags": ["alan garcia", "politica"]
-				            },
+				            
 				            {
 				                "idNoticia": 3,
 				                "codigoNoticia": "TFH-1455",
@@ -44,30 +17,7 @@
 				                "categoria": "Economia",
 				                "tags": ["mestiza", "gamarra"]
 				            },
-				            {
-				                "idNoticia": 4,
-				                "codigoNoticia": "TFS-7890",
-				                "tituloNoticia": "Esta semana continua el torneo Clausura",
-				                "descripcionNoticia": "Loryade laborum magni, officiis ratione reiciendis sunt aque Commodi doloribus error ex fuos est fuga illum iusto ne" +
-				                "velit voluptas voluptates. Dst fuga illum iusto necessita in sed vitae voluptates. Tenetur giat laborum mag At e",
-				                "fechaPublicacion": new Date(2015,10,2),
-				                "banner" : "http://cde.3.elcomercio.pe/ima/0/1/2/3/5/1235138/base_image.jpg",
-				                "destacado": false,
-				                "categoria": "Deportes",
-				                "tags": ["futbol", "clausura"]
-				            },
-				            {
-				                "idNoticia": 5,
-				                "codigoNoticia": "JNQ-4564",
-				                "tituloNoticia": "Avion ruso que cayo en Egipto se despedazo",
-				                "descripcionNoticia": "Loryade laborum magni, officiis ratione reiciendis sunt aque Commodi doloribus error ex fuos est fuga illum iusto ne" +
-				                "velit voluptas voluptates. Dst fuga illum iusto necessita in sed vitae voluptates. Tenetur giat laborum mag At e",
-				                "fechaPublicacion": new Date(2015,10,3),
-				                "banner" : "http://cde.3.elcomercio.pe/ima/0/1/2/3/5/1235130/base_image.jpg",
-				                "destacado": false,
-				                "categoria": "Mundo",
-				                "tags": ["avion", "accidente"]
-				            },
+				            
 				            {
 				                "idNoticia": 6,
 				                "codigoNoticia": "NRR-6002",
