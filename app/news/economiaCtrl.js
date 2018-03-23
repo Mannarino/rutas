bc34883f7be9c0921ce26b1cbbd73c07609d912e
frontend.moises@gmail.com <frontend.moises@gmail.com>
2018-03-23 02:34:35 -0300

    qaa

--- app/news/economiaCtrl.js
+++ app/news/economiaCtrl.js
@@ -7,13 +7,13 @@
 				            {
 				                "idNoticia": 1,
 				                "codigoNoticia": "NWD-9838",
-				                "tituloNoticia": "Peru envio nota diplomatica a Chile",
+				                "tituloNoticia": "Crecimiento economico",
 				                "descripcionNoticia": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dolor,eos laborum quaerat quam quos rerum!" +
 				                "Expedita itaque optio praesentium quo sequi soluta voluptatem! At eos est fuga illum iusto necessitatibus " +
 				                "bcaecati quia tempora ullam. Commodi doloribus error ex fugiat laborum magni, officiis ratione reiciendis sunt" +
 				                "velit voluptas voluptates. Culpa deleniti dolore, excepturi fugit id in sed vitae voluptates. Tenetur.",
 				                "fechaPublicacion": new Date(),
-				                "banner" : "img/economista-a.jpg",
+				                "banner" : "img/economia-a.jpg",
 				                "destacado": true,
 				                "categoria": "Politica",
 				                "tags": ["peru", "politica", "guerra"]
@@ -21,13 +21,13 @@
 				            {
 				                "idNoticia": 2,
 				                "codigoNoticia": "NFK-1275",
-				                "tituloNoticia": "Alan Garcia cuestiona ofertas laborales",
+				                "tituloNoticia": "Ofertas laborales",
 				                "descripcionNoticia": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Al quaerat quam quos rerum!" +
 				                "Expedita itaque Commodi doloribus error ex fugiat laborum mag At eos est fuga illum iusto necessitatibus " +
 				                "bcaecati quia tempora ullam. Commodi doloribus error ex fugiat laborum magni, officiis ratione reiciendis sunt" +
 				                "velit voluptas voluptates. Dst fuga illum iusto necessita in sed vitae voluptates. Tenetur.",
 				                "fechaPublicacion": new Date(2015,0,1),
-				                "banner" : "img/economista-b.jpg",
+				                "banner" : "img/economia-b.jpg",
 				                "destacado": true,
 				                "categoria": "Politica",
 				                "tags": ["alan garcia", "politica"]
