5fd53becad89bbbf534475377e0d9cbba789b990
frontend.moises@gmail.com <frontend.moises@gmail.com>
2018-03-23 00:52:39 -0300

    jnng

--- app/rutas.js
+++ app/rutas.js
@@ -2,26 +2,31 @@ angular.module('newsWiki', ['ngRoute'])
 .config(function($routeProvider){
  	$routeProvider.when('/', {
 		controller: 'NewsCtrl',
+		controllerAs: "vm",
 		templateUrl: 'app/partials/inicio.html'
 	})
  
 	.when('/deporte', {
 		controller: 'DeporteCtrl',
+		controllerAs: "vm",
 		templateUrl: 'app/partials/deporte.html'
 	})
  
 	.when('/moda', {
 		controller: 'ModaCtrl',
+		controllerAs: "vm",
 		templateUrl: 'app/partials/moda.html'
 	})
  	
  	.when('/economia', {
 		controller: 'EconomiaCtrl',
+		controllerAs: "vm",
 		templateUrl: 'app/partials/economia.html'
 	})
 
  	.when('/politica', {
 		controller: 'PoliticaCtrl',
+		controllerAs: "vm",
 		templateUrl: 'app/partials/politica.html'
 	})
 
